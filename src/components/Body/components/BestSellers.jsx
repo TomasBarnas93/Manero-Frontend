15b2e5573b6d2a1f68de7dc4a2d3c299ef7aa6e7
TomasBarnas93 <btomek506@gmail.com>
2023-05-11 09:40:03 +0200

    cardFix

--- src/components/Body/components/BestSellers.jsx
+++ src/components/Body/components/BestSellers.jsx
@@ -5,11 +5,13 @@ import ProductItem from "./ProductItem";
 const BestSellers = () => {
   const products = useContext(ProductContext);
 
+  const limitedProducts = products.slice(0, 4);
+
   return (
-    <div className="mt-5 mb-5">
+    <div className="mt-5 mb-5 ml-2">
       <h2>Best Sellers</h2>
-      <ul className="flex gap-4">
-        {products.map((product) => (
+      <ul className="flex gap-6">
+        {limitedProducts.map((product) => (
           <li key={product.id}>
             <ProductItem product={product} />
           </li>
--- src/components/Body/components/ProductItem.jsx
+++ src/components/Body/components/ProductItem.jsx
@@ -2,32 +2,22 @@ import React from "react";
 
 const ProductItem = ({ product }) => {
   return (
-    <div className="bg-white rounded-lg shadow-md p-4 h-66 w-100 flex flex-col justify-between">
-      <div className="flex flex-col justify-between h-full">
-        <div>
-          <img
-            className="w-32 h-32 object-cover"
-            src={product.imageUrl}
-            alt="Img"
-          />
-          <div className="mt-2">
-            <h3 className="text-xl font-semibold">
-              <span className="d-inline-block d-sm-none">{product.name}</span>
-              <span className="d-none d-sm-inline-block">{product.name}</span>
-            </h3>
-            <p className="text-gray-500">
-              <span className="d-inline-block d-sm-none">
-                {product.description}
-              </span>
-              <span className="d-none d-sm-inline-block">
-                {product.description}
-              </span>
-            </p>
-          </div>
-        </div>
-        <div>
-          <p className="text-gray-500 text-sm">{product.price}</p>
-        </div>
+    <div className="bg-slate-200 rounded-lg shadow-md p-4 h-64 w-64 ;">
+      <div className="flex justify-center">
+      <img
+        className="h-32"
+        src={product.imageUrl}
+        alt="Img"
+      />
+      </div>
+      <div>
+      <div>
+        <h3 className="text-center underline underline-offset-8 font-bold">{product.name}</h3>
+        <p className="text-center">{product.description}</p>
+      </div>
+      <div>
+        <p className="text-center font-bold">{product.price}$</p>
+      </div>
       </div>
     </div>
   );
