91bfec0d8c21e41a428a7b1cc0fd94f76e115347
Jesper Åström <Jesper.Astrom@yh.nackademin.se>
2023-05-11 14:28:54 +0200

    Added karusell

--- src/components/Body/components/ShowCase.jsx
+++ src/components/Body/components/ShowCase.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 const ShowCase = () => {
   return (
-    <div className='text-white font-sans font-semibold text-4xl p-3 h-[33vh] bg-[#D7DFF3] border-y border-gray-300 lg:text-center'>
-        <div className="mt-28">
-            Welcome to
-            <p>Manero</p>
-            <p>50% off</p>
-        </div>
+    <div className="text-white font-sans font-semibold text-4xl p-3 h-[33vh] bg-[#D7DFF3] border-y border-gray-300 lg:text-center flex items-end">
+      <div className="flex flex-col justify-end h-full">
+        <p>Welcome to</p>
+        <p>Manero</p>
+        <p>50% off</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ShowCase
+export default ShowCase;
--- src/components/Body/views/Home.jsx
+++ src/components/Body/views/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ShowCase from '../components/ShowCase'
 import SmallShowCase from '../components/SmallShowCase'
 import CollectionProducts from '../components/CollectionProducts'
+import Carousel from '../components/Carousel'
 
 const Home = () => {
   return (
     <>
         <ShowCase/>
+        <Carousel/>
         {/* Skickar in lista med BestSellers via api */}
         <CollectionProducts title='Best sellers' api='bestsellers'/>
         <SmallShowCase/>
