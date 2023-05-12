727afe7b321edea344ae23a3988719b87df74195
Eftiand <kristianjer@gmail.com>
2023-05-12 19:25:07 +0200

    Login screen template done.

---
+++ src/components/Body/views/Account.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Login from './Login'
+
+const Account = () => {
+  return (
+    <main>
+
+        {/* Here we will redirect the user to login if they are not logged in. */}
+        <Login/>
+    </main>
+
+
+  )
+}
+
+export default Account
--- src/components/misc/InputBox.jsx
+++ src/components/misc/InputBox.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 //behöver mer tailwind css!
-const InputBox = ({ label, type, id, placeholder, value, onChange, required }) => {
+const InputBox = ({ label, type, id, placeholder, value, onChange, required, classes }) => {
   return (
-    <div className="w-1/2 mx-auto mb-4">
+  <div className="w-100 mx-auto mb-4">
     <label className="block font-medium text-gray-700 mb-1">{label}</label>
     <input
-      className="form-input w-full px-4 py-2 rounded border-gray-400 border-2 focus:border-blue-500 focus:outline-none"
+      className={`form-input w-full px-4 py-2 rounded-3xl border-gray-200 border-2 focus:border-blue-500 focus:outline-none ${classes}`}
+      id={id}
       type={type}
       value={value}
       onChange={onChange}
