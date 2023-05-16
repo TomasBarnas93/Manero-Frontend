afd75bf75d870f7d80c5c4b18dd8fc87e4515204
Eftiand <kristianjer@gmail.com>
2023-05-16 14:23:07 +0200

    ID431, Header och registrering klar

--- src/components/Body/views/Account.jsx
+++ src/components/Body/views/Account.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Login from './Login'
+import Register from './Register'
 
 const Account = () => {
   return (
     <main>
 
         {/* Here we will redirect the user to login if they are not logged in. */}
-        <Login/>
+        {/* <Login/> */}
+
+        <Register />
+
     </main>
 
 
--- src/components/misc/Icon.jsx
+++ src/components/misc/Icon.jsx
@@ -3,12 +3,11 @@ import { NavLink } from 'react-router-dom';
 
 const Icon = (props) => {
 
-
   return (
 
       <NavLink
         to={props.to}
-        className={`${props.button} rounded-3xl text-center`}
+        className={`${props.button} ${props.active===props.to ? "bg-gray-200" : ""} rounded-3xl text-center inline-block }`}
       >
         <i className={`${props.icon}`}></i>
       </NavLink>
