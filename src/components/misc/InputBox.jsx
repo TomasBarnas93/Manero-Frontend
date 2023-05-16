37ec3cfe4f9f172ad7d318c75fe6e4677ea28841
mattekiwi <kivimatte@gmail.com>
2023-05-16 14:58:45 +0200

    inputbox updated

--- src/components/misc/InputBox.jsx
+++ src/components/misc/InputBox.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-//behöver mer tailwind css!
+
 const InputBox = ({ label, type, id, placeholder, value, onChange, required, classes }) => {
   return (
-  <div className="w-100 mx-auto mb-4">
-    <label className="block font-medium text-gray-700 mb-1">{label}</label>
+  <div className="w-100 mx-auto mb-4 relative px-2 mt-8">
+    <label className="block font-medium text-gray-700 mb-1 absolute -top-4 left-5 bg-white px-2 z-10">{label}</label>
     <input
       className={`form-input w-full px-4 py-2 rounded-3xl border-gray-200 border-2 focus:border-blue-500 focus:outline-none ${classes}`}
       id={id}
