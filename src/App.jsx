5f67e616741bc15a59d7ccc81a54e8b1d12caafa
Eftiand <kristianjer@gmail.com>
2023-05-18 19:58:23 +0200

    Intro, fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 import { ProductProvider } from './contexts/ProductProvider';
 import Body from './components/Body/Body';
 import { Header } from './components/Header/Header';
+import { presentIntro } from './js/Intro';
 
 function App() {
+
+  presentIntro();
+
   return (
     <ProductProvider>
       <Header />
