a55cc1d2adb9f184ce2578d402136ee62bf00b7d
Adis Bacic <114671655+AdisBacic@users.noreply.github.com>
2023-05-24 15:40:04 +0200

    merge conflict fixed

--- src/components/Body/views/Details.jsx
+++ src/components/Body/views/Details.jsx
@@ -1,47 +1,47 @@
-  import React, { useState, useContext, useEffect } from 'react';
-  import { useParams } from 'react-router-dom';
-  import StarRating from '../../misc/StarRating';
-  import { ProductContext } from '../../../contexts/ProductProvider';
-  import Reviews from '../../misc/Reviews';
-  import { ReviewProvider } from '../../../contexts/ReviewProvider';
+import React, { useState, useContext, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import StarRating from '../../misc/StarRating';
+import { ProductContext } from '../../../contexts/ProductProvider';
+import Reviews from '../../misc/Reviews';
+import { ReviewProvider } from '../../../contexts/ReviewProvider';
 
 function Details() {
   const { id } = useParams();
   const { fetchSingleProduct } = useContext(ProductContext);
   const [counter, setCounter] = useState(1);
-  const [product, setProduct] = useState(null); 
+  const [product, setProduct] = useState(null);
 
-    const plusCounter = () => {
-      setCounter(prevCounter => prevCounter + 1);
-    };
+  const plusCounter = () => {
+    setCounter(prevCounter => prevCounter + 1);
+  };
 
-    const minusCounter = () => {
-      if (counter > 1) {
-        setCounter(prevCounter => prevCounter - 1);
-      }
-    };
-
-    const productSizeList = ["XXL", "XL", "S", "M", "L", "XS"];
-    const colorOptions = ["#ff6262", "#63c7ff", "#f8e7cd", "#323858", "#111111"];
-
- useEffect(() => {
-  const fetchData = async () => {
-    try {
-      const data = await fetchSingleProduct(id);
-      setProduct(data);
-    } catch (error) {
-      console.error('Error fetching product:', error);
+  const minusCounter = () => {
+    if (counter > 1) {
+      setCounter(prevCounter => prevCounter - 1);
     }
   };
 
-  fetchData();
-}, [id]);
+  const productSizeList = ["XXL", "XL", "S", "M", "L", "XS"];
+  const colorOptions = ["#ff6262", "#63c7ff", "#f8e7cd", "#323858", "#111111"];
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetchSingleProduct(id);
+        setProduct(data);
+      } catch (error) {
+        console.error('Error fetching product:', error);
+      }
+    };
+
+    fetchData();
+  }, [id]);
 
   if (product === null) {
     return <>loading..</>;
   }
 
-console.log(product);
+  console.log(product);
 
   return (
     <div className="container px-4 mx-auto">
@@ -52,61 +52,60 @@ console.log(product);
           className="rounded-lg shadow-md p-4 h-64 w-full lg:w-80"
         />
       </div>
-        </div>
-        <div className="pt-4">
-          <div className='flex gap-24'>
+      <div className="pt-4">
+        <div className='flex gap-24'>
           <h2 className="font-bold">{product.name}</h2>
           <button className="link">
-              <i className="fa-regular fa-heart opacity-50"></i>
-            </button>
-          </div>
-          <StarRating rating={product.starRating} />
-          <div className='flex gap-48'>
+            <i className="fa-regular fa-heart opacity-50"></i>
+          </button>
+        </div>
+        <StarRating rating={product.starRating} />
+        <div className='flex gap-48'>
           <p>${product.price}</p>
           <div className='flex gap-2'>
             <button onClick={minusCounter}>-</button>
             <span>{counter}</span>
             <button onClick={plusCounter}>+</button>
           </div>
-          </div>
-          <div className="mt-3">
-            <p>Size</p>
-            <div className="flex pt-2">
-              {productSizeList.map((size) => (
-                <button
-                  key={size}
-                  className="w-10 h-10 flex items-center justify-center border border-gray-400 rounded-full mr-2"
-                >
-                  {size}
-                </button>
-              ))}
-            </div>
-          </div>
-          <div className="pt-4 flex">
-            <p>Color</p>
-            <div className="flex pl-5">
-              {colorOptions.map((color) => (
-                <button
-                  key={color}
-                  className="w-8 h-8 rounded-full mr-2"
-                  style={{ backgroundColor: color }}
-                ></button>
-              ))}
-            </div>
-          </div>
-          <div className='mt-5'>
-              <p>Description</p>
-              <p className='text-zinc-400 font-light'>{product.description}</p>
-          </div>
-          <button className='bg-black hover:bg-blue-600 text-white w-80 py-2 rounded-3xl mt-11'>+ ADD TO CART</button>
-
-          <ReviewProvider productId={id}>
-            <Reviews />
-          </ReviewProvider>
-
         </div>
-      </div>
-    );
-  }
+        <div className="mt-3">
+          <p>Size</p>
+          <div className="flex pt-2">
+            {productSizeList.map((size) => (
+              <button
+                key={size}
+                className="w-10 h-10 flex items-center justify-center border border-gray-400 rounded-full mr-2"
+              >
+                {size}
+              </button>
+            ))}
+          </div>
+        </div>
+        <div className="pt-4 flex">
+          <p>Color</p>
+          <div className="flex pl-5">
+            {colorOptions.map((color) => (
+              <button
+                key={color}
+                className="w-8 h-8 rounded-full mr-2"
+                style={{ backgroundColor: color }}
+              ></button>
+            ))}
+          </div>
+        </div>
+        <div className='mt-5'>
+          <p>Description</p>
+          <p className='text-zinc-400 font-light'>{product.description}</p>
+        </div>
+        <button className='bg-black hover:bg-blue-600 text-white w-80 py-2 rounded-3xl mt-11'>+ ADD TO CART</button>
 
-  export default Details;
+        <ReviewProvider productId={id}>
+          <Reviews />
+        </ReviewProvider>
+
+      </div>
+    </div>
+  );
+}
+
+export default Details;
