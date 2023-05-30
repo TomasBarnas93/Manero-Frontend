fe1727c90d8cc60a24f2d89d2f06054770393b4c
Eftiand <kristianjer@gmail.com>
2023-05-30 09:14:32 +0200

    Update details  5

--- src/components/Body/views/MyAddress.jsx
+++ src/components/Body/views/MyAddress.jsx
@@ -1,16 +1,5 @@
-import {useState} from 'react'
-import InputBox from '../../misc/InputBox';
 
 const MyAddress = () => {
-    const [title, setTitle] = useState('');
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [addressLine1, setAddressLine1] = useState('');
-    const [postalCode, setPostalCode] = useState('');
-    const [city, setCity] = useState('');
-
-
-
 
 
 
